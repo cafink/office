2c2cf05134fdc367c949411ad777cf3040ebbd34
Carl Fink <cafink@gmail.com>
2015-07-29 17:28:04 -0500

    Add SQL for database structure.

---
+++ sql/office.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`first_name` varchar(255) NOT NULL,
+	`last_name` varchar(255) NOT NULL,
+	`email` varchar(255) DEFAULT NULL,
+	`color` varchar(255) DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `ping-pong` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`price` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `seats` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`type` varchar(255) NOT NULL,
+	`price` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `offices` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`type` varchar(255) NOT NULL,
+	`price` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `slots` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`service` enum('ping-pong', 'seat', 'office') NOT NULL,
+	`service_id` int(11),
+	`time` timestamp NOT NULL,
+	`quantity` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `purchases` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`user_id` int(11),
+	`slot_id` int(11),
+	PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
