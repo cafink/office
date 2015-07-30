e60accbf2ef07b3bc9508ef637afe12971801883
Carl Fink <cafink@gmail.com>
2015-07-30 00:17:23 -0500

    Create login form.

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+	include 'lib/User.php';
+
+	if(isset($_POST['submit'])) {
+
+		$user = User::find('email', $_POST['email'], true);
+
+		if (isset($user)) {
+
+			$success = $user->checkPassword($_POST['password']);
+
+			if ($success) {
+				session_start();
+				$_SESSION['user'] = $user;
+				header('Location: index.php');
+				die();
+			}
+		}
+
+		$errors = 'Invalid username/password combination.';
+	}
+
+?>
+
+<?php ob_start(); ?>
+
+<h1>Log In</h1>
+
+<?php include 'templates/errors.php'; ?>
+
+<form action="login.php" method="post">
+
+	<div>
+		<label for="email">e-mail address:</label><br />
+		<input type="text" name="email" />
+	</div>
+
+	<div>
+		<label for="password">password:</label><br />
+		<input type="password" name="password" />
+	</div>
+
+	<div><input type="submit" name="submit" value="Login"></div>
+
+
+</form>
+
+<?php
+	$page['content'] = ob_get_clean();
+	include 'templates/layout.php';
+?>
