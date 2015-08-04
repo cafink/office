5ebbaa6793dd9ffbf94b77ebc32c8f12a3b0cd58
Carl Fink <cafink@gmail.com>
2015-08-03 23:52:46 -0500

    Display order form's service field only for service types with multiple services.

--- order.php
+++ order.php
@@ -4,29 +4,37 @@
 	include 'lib/requireLogin.php';
 
 	ob_start();
+
+	$service_type = $_GET['service'];
+	$service_class = Slot::className($service_type);
 ?>
+
+<h1><?php echo ucwords($service_type, '-'); ?> Order Form</h1>
 
 <form action="order.php" method="post">
 
-	<div>
-		<label for="service">service:</label><br />
-		<select name="service" id="service">
-			<option value="">- select -</option>
-			<?php
-
-				$service_type = $_GET['service'];
-
-				$services = Slot::availableServices($service_type);
-				foreach ($services as $service)
-					echo '<option value="' . $service->id . '">' . $service->name . '</option>'
-			?>
-		</select>
-	</div>
+	<?php
+		if ($service_class::$single) {
+			$services = array($service_class::find(null, null, true));
+		} else {
+	?>
+		<div>
+			<label for="service">service:</label><br />
+			<select name="service" id="service">
+				<option value="">- select -</option>
+				<?php
+					$services = Slot::availableServices($service_type);
+					foreach ($services as $service)
+						echo '<option value="' . $service->id . '">' . $service->name . '</option>'
+				?>
+			</select>
+		</div>
+	<?php } ?>
 
 	<?php foreach ($services as $service) { ?>
-		<div class="slot" data-service-id="<?php echo $service->id; ?>" style="display: none;">
-			<label for="slot"><?php echo $service->name; ?> slot:</label></br />
-			<select name="slot" disabled>
+		<div class="slot" data-service-id="<?php echo $service->id; ?>"<?php if (!$service_class::$single) echo ' style="display: none;"'; ?>>
+			<label for="slot"><?php if (isset($service->name)) echo $service->name . ' '; ?>slot:</label></br />
+			<select name="slot"<?php if (!$service_class::$single) echo ' disabled'; ?>>
 				<option value="">- select -</option>
 				<?php
 					$slots = Slot::available($service_type, $service->id);
@@ -41,20 +49,22 @@
 
 </form>
 
-<script type="text/javascript">
-	$(function() {
-		$('#service').change(function() {
+<?php if (!$service_class::$single) { ?>
+	<script type="text/javascript">
+		$(function() {
+			$('#service').change(function() {
 
-			// Hide and disable all slot dropdowns.
-			$('.slot').hide();
-			$('.slot select').prop('disabled', true);
+				// Hide and disable all slot dropdowns.
+				$('.slot').hide();
+				$('.slot select').prop('disabled', true);
 
-			// Re-enable and display the one for the selected service.
-			$('.slot[data-service-id="' + $(this).val() + '"] select').prop('disabled', false);
-			$('.slot[data-service-id="' + $(this).val() + '"]').show();
+				// Re-enable and display the one for the selected service.
+				$('.slot[data-service-id="' + $(this).val() + '"] select').prop('disabled', false);
+				$('.slot[data-service-id="' + $(this).val() + '"]').show();
+			});
 		});
-	});
-</script>
+	</script>
+<?php } ?>
 
 <?php
 	$page['title'] = 'Order Form';
