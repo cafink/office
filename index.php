08cad1327940ec6ae04f863bc3d49f258757c34f
Carl Fink <cafink@gmail.com>
2015-08-05 13:51:14 -0500

    Add order form links to home page.

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
+
 	include 'lib/requireLogin.php';
+
+	include_once 'lib/Slot.php';
+
 	ob_start();
+
+	$service_types = Slot::availableServiceTypes();
 ?>
 
-<p>Hello, <?php echo $_SESSION['user']->first_name; ?></p>
+<ul>
+	<?php foreach ($service_types as $service_type) { ?>
+		<li><a href="order.php?service=<?php echo $service_type; ?>"><?php echo ucwords($service_type, '-'); ?> Order Form</a></li>
+	<?php } ?>
+</ul>
 
 <?php
 	$page['content'] = ob_get_clean();
